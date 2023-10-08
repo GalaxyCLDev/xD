d8849910529f3a45ebf8f1c98150b4167d7a076a
GalaxyCLDev <118214204+GalaxyCLDev@users.noreply.github.com>
2023-10-08 04:11:59 -0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,5 +1,6 @@
 -- Verificar si el jugador tiene suficientes puntos y el nivel requerido para las bonificaciones
 if playerLevel > 0 then
+    print("Player Level:", playerLevel)    
     -- Aplicar la bonificación correspondiente para la vocación del jugador
     local function applyBonus(condition, message)
         player:addCondition(condition)
@@ -9,7 +10,9 @@ if playerLevel > 0 then
 
     if playerVocation == 1 then -- sorcerer
         if playerLevel == 1 and playerPoints >= levels[playerLevel].requiredPoints then
+            print("Applying Level 1 Bonus")
             applyBonus(conditions.sorcerer, "Conseguiste el Nivel 1 de Buffos de Sorcerer. (50+ velocidad, +500hp +500mana)!")
         elseif playerLevel >= 2 and playerPoints >= levels[2].requiredPoints then
+            print("Applying Level 2 Bonus")
             applyBonus(conditions.sorcerer2, "Conseguiste el Nivel 2 de Buffos de Sorcerer. (120+ velocidad, +1500hp +1500mana)!")
         end
